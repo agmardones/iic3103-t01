e9e72285acf86e9bf2a09368646f4396d35b9328
Andrés Mardones <agmardones@uc.cl>
2019-03-24 18:41:51 -0300

    Film view with its resources

--- src/FilmView.js
+++ src/FilmView.js
@@ -1,22 +1,111 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import { getFilmInfo } from "./helpers";
+import {
+  Typography,
+  withStyles,
+  ListItem,
+  List,
+  ListItemText,
+  ListSubheader,
+  Paper,
+  Grid
+} from "@material-ui/core";
 
+const styles = {
+  listRoot: {
+    marginTop: 25,
+    height: "300px",
+    overflow: "scroll",
+    paddingBottom: 20
+  }
+};
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      info: []
+      film: null,
+      characters: [],
+      planets: [],
+      starships: []
     };
   }
 
+  componentWillMount() {
+    getFilmInfo(this.props.match.params.id).then(response => {
+      const { film, characters, planets, starships } = response;
+      this.setState({ film, characters, planets, starships });
+    });
+  }
+
   render() {
-    const filmId = this.props.match.params.id;
+    const { classes } = this.props;
+    const { film, characters, planets, starships } = this.state;
     return (
       <div>
-        <h1> {`Film: ${filmId}`}</h1>
+        {film && <Typography variant="h2">{film.title}</Typography>}
+        <Grid direction="row" container spacing={24}>
+          {film && characters && (
+            <Grid item xs={12} md={6} lg={6}>
+              <Paper className={classes.listRoot}>
+                <List
+                  subheader={
+                    <ListSubheader disableSticky={true} component="div">
+                      Personajes
+                    </ListSubheader>
+                  }
+                >
+                  {characters.map((char, idx) => (
+                    <ListItem key={idx}>
+                      <ListItemText primary={`${char}`} />
+                    </ListItem>
+                  ))}
+                </List>
+              </Paper>
+            </Grid>
+          )}
+          {film && planets && (
+            <Grid item xs={12} md={6} lg={6}>
+              <Paper className={classes.listRoot}>
+                <List
+                  subheader={
+                    <ListSubheader disableSticky={true} component="div">
+                      Planetas
+                    </ListSubheader>
+                  }
+                >
+                  {planets.map((planet, idx) => (
+                    <ListItem key={idx}>
+                      <ListItemText primary={`${planet}`} />
+                    </ListItem>
+                  ))}
+                </List>
+              </Paper>
+            </Grid>
+          )}
+          {film && starships && (
+            <Grid item xs={12} md={6} lg={6}>
+              <Paper className={classes.listRoot}>
+                <List
+                  subheader={
+                    <ListSubheader disableSticky={true} component="div">
+                      Naves
+                    </ListSubheader>
+                  }
+                >
+                  {starships.map((starship, idx) => (
+                    <ListItem key={idx}>
+                      <ListItemText primary={`${starship}`} />
+                    </ListItem>
+                  ))}
+                </List>
+              </Paper>
+            </Grid>
+          )}
+        </Grid>
       </div>
     );
   }
 }
 
-export default withRouter(App);
+export default withRouter(withStyles(styles)(App));
--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,13 @@
+const fetchEntities = async entitiesArray => {
+  const entitiesInfo = await Promise.all(
+    entitiesArray.map(entity => fetch(entity))
+  );
+  const parsedEntity = await Promise.all(
+    entitiesInfo.map(entity => entity.json())
+  );
+  return parsedEntity;
+};
+
 export const getAllFilms = async () => {
   const rawFilms = await fetch("https://swapi.co/api/films");
   const films = await rawFilms.json();
@@ -21,4 +31,16 @@ export const getAllFilms = async () => {
       url
     };
   });
+};
+
+export const getFilmInfo = async filmId => {
+  const rawFilm = await fetch(`https://swapi.co/api/films/${filmId}`);
+  const film = await rawFilm.json();
+  const charactersInfo = await fetchEntities(film.characters);
+  const planetsInfo = await fetchEntities(film.planets);
+  const starshipsInfo = await fetchEntities(film.starships);
+  const characters = charactersInfo.map(char => char.name);
+  const planets = planetsInfo.map(planet => planet.name);
+  const starships = starshipsInfo.map(starship => starship.name);
+  return { film, characters, planets, starships };
 };
